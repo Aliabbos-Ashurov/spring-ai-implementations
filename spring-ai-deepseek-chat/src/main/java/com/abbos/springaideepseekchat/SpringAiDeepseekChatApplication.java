f1ccd8a2e5778812cc1473baef8e2e1f985dedfc
Aliabbos <aliabbosashurov@gmail.com>
2025-02-07 10:52:27 +0500

    ....

---
+++ spring-ai-deepseek-chat/src/main/java/com/abbos/springaideepseekchat/SpringAiDeepseekChatApplication.java
@@ -0,0 +1,12 @@
+package com.abbos.springaideepseekchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDeepseekChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDeepseekChatApplication.class, args);
+    }
+}
